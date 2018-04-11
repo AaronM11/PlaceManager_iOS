77a92eaf458104efede4ee1cdef3ccddc8548905
Aaron <a.musengo2@gmail.com>
2018-04-10 20:14:15 -0700

    Initial Commit for place manager

---
+++ Place Manager/Place Manager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Place Manager
+//
+//  Created by Aaron Musengo on 4/10/18.
+//  Copyright © 2018 Aaron Musengo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
